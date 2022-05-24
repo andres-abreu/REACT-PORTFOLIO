37090f68db05cd924a2c02f4deb143f93fb71ded
Andres: Abreu <andresabreu@andress-MacBook-Pro.local>
2022-05-24 19:01:22 -0400

    updated-projects

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="mt-auto">
       <footer className="d-flex justify-content-around p-2">
-        <h4 className="my-auto text-center fw-bold">Connect with me ---></h4>
+        <h4 className="my-auto text-center fw-bold">Lets connect on-line </h4>
         <ul className="list-inline my-auto text-center">
           <li className="list-inline-item fw-bold"><a className="text-dark" target="_blank" rel="noreferrer" href="https://github.com/andres-abreu">GitHub</a></li>
           <li className="list-inline-item fw-bold"><a className="text-dark" target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/andresabreucreator/">LinkedIn</a></li>
--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -4,40 +4,50 @@ function Portfolio() {
   const projects = [
     {
       name: "Budget Tracker",
-      id: "budget-tracker",
-      description: "As the name states, this is a simple finance-ledger, utilizaing indexedDB & service-worker-functionality which provides a mobile-first PWA user-experience.",
+      id: "budget-tracker-screen-shot",
+      description:
+        "As the name states, this is a simple finance-ledger, utilizaing indexedDB & service-worker-functionality which provides a mobile-first PWA user-experience.",
       liveURL: "https://stormy-scrubland-05355.herokuapp.com/",
-      gitHub: "https://github.com/andres-abreu/BUDGET-TRACKER"
+      gitHub: "https://github.com/andres-abreu/BUDGET-TRACKER",
     },
 
     {
       name: "Weather Dashboard",
-      id: "weather-dashboard",
-      description: "This is a weather API that indicates the current weather at your chosen location.",
+      id: "weather-dashboard_Screen Shot",
+      description:
+        "This is a weather API that indicates the current weather at your chosen location.",
       liveUrl: "https://andres-abreu.github.io/WEATHER-DASHBOARD/",
-      gitHub: "https://github.com/andres-abreu/WEATHER-DASHBOARD"
+      gitHub: "https://github.com/andres-abreu/WEATHER-DASHBOARD",
     },
-
-]    
-return (
-  <section>
-    <h2 className="text-center fw-bold my-3">My Projects</h2>
-    {projects.map((proj, i)=>(
-    <div key={i} className="d-flex flex-column m-5 p-3 my-border card">
-      <div className="text-center">
-        <img className="p-image" src={require(`../../assets/images ${proj.id}.png`)} alt="Project Screenshot"></img>
-      </div>
-      <div className="mx-auto text-center">
-        <h3 className="fw-bold m-2">{proj.name}</h3>
-        <p>{proj.description}</p>
-        <a href={proj.liveURL} target="_blank" rel="noreferrer">View Deployed Application</a> <br></br>
-        <a href={proj.gitHub} target="_blank" rel="noreferrer">View GitHub Repository</a>
-      </div>
-    </div>
-    ))}
-     {/*  */}
-   </section>
-);
+  ];
+  return (
+    <section>
+      <h2 className="text-center fw-bold my-3">My Projects</h2>
+      {projects.map((proj, i) => (
+        <div key={i} className="d-flex flex-column m-5 p-3 my-border card">
+          <div className="text-center">
+            <img
+              className="p-image"
+              src={require(`../../assets/images/${proj.id}.png`)}
+              alt="Project Screenshot"
+            ></img>
+          </div>
+          <div className="mx-auto text-center">
+            <h3 className="fw-bold m-2">{proj.name}</h3>
+            <p>{proj.description}</p>
+            <a href={proj.liveURL} target="_blank" rel="noreferrer">
+              View Deployed Application
+            </a>{" "}
+            <br></br>
+            <a href={proj.gitHub} target="_blank" rel="noreferrer">
+              View GitHub Repository
+            </a>
+          </div>
+        </div>
+      ))}
+      {/*  */}
+    </section>
+  );
 }
 
 export default Portfolio;
