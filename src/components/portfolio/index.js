c551a90c22a895a96a2d1f933af2df7ce790487f
Andres Abreu <andresabreu@andress-MacBook-Pro.local>
2022-06-17 10:40:19 -0400

    portfolio-desc.-mods

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -6,7 +6,7 @@ function Portfolio() {
       name: "Budget Tracker",
       id: "budget-tracker-screen-shot",
       description:
-        "As the name states, this is a simple finance-ledger, utilizaing indexedDB, NoSQL & express technologies with service-worker-functionality which provides a mobile-first PWA user-experience.",
+        "This was a solo project and as the name states, this is a simple finance-ledger, utilizaing indexedDB, NoSQL & express technologies with service-worker-functionality which provides a mobile-first PWA user-experience.",
       liveURL: "https://stormy-scrubland-05355.herokuapp.com/",
       gitHub: "https://github.com/andres-abreu/BUDGET-TRACKER",
     },
@@ -15,7 +15,7 @@ function Portfolio() {
       name: "Weather Dashboard",
       id: "weather-dashboard_Screen Shot",
       description:
-        "This is a third-party-weather-API that indicates the current weather at your chosen location, using the local-storage to store any persistent data.",
+        "This was a solo-project using a third-party-weather-API that indicates the current weather at your chosen location, using the local-storage to store any persistent data.",
       liveURL: "https://andres-abreu.github.io/WEATHER-DASHBOARD/",
       gitHub: "https://github.com/andres-abreu/WEATHER-DASHBOARD"
     },
@@ -24,7 +24,7 @@ function Portfolio() {
       name: "Love Calculator",
       id: "love-calculator-screen-shot",
       description:
-        "This love calculator was created using three separate API's, from Spotify, Pexels and RapidAPI's love- calculator. Within this project we also used the following CDNS: Jquery (for shorter code), Materialize(for faster UX/UI) and Google fonts.",
+        "This love calculator was created by a team of four using three separate API's, from Spotify, Pexels and RapidAPI's love- calculator. Within this project we also used the following CDNS: Jquery (for shorter code), Materialize(for faster UX/UI) and Google fonts.",
       liveURL: "https://zela7alex.github.io/love-match-calculator/",
       gitHub: "https://github.com/andres-abreu/LOVE-MATCH-CALCULATOR"
     },
@@ -32,7 +32,7 @@ function Portfolio() {
     {
       name: "Technology Blog",
       id: "tech-blog-screen-shot",
-      description: "This blog-post allows users to create and share technology-focused-post, users may view, edit, delete & comment. This app follows the model-view-controller[MVC] paradigm in its archetictural structure using handlebars.js as the templating language, Sequelize as the object-relational-mapping[ORM], and express-session-npm-package for authentication.",
+      description: "This was a solo-blog-post-project in which allows users to create and share technology-focused-post, users may view, edit, delete & comment. This app follows the model-view-controller[MVC] paradigm in its archetictural structure using handlebars.js as the templating language, Sequelize as the object-relational-mapping[ORM], and express-session-npm-package for authentication.",
       liveURL: "https://stark-bastion-74188.herokuapp.com/dashboard",
       gitHub: "https://github.com/andres-abreu/TECH-BLOG",
     },
@@ -41,7 +41,7 @@ function Portfolio() {
       name: "Note Taker",
       id: "note-taker-screen-shot",
       description:
-      "This is a simple note-taking-app that can save and retreive data from a JSON-file using express.js on the back-end.",
+      "This solo-project is a simple note-taking-app that can save and retreive data from a JSON-file using express.js on the back-end.",
       liveURL: "https://glacial-wildwood-22711.herokuapp.com/",
       gitHub: "https://github.com/andres-abreu/NOTE-TAKER"
     },
@@ -49,7 +49,7 @@ function Portfolio() {
     {
       name: "Work Day Scheduler",
       id: "work-day-scheduler-screen-shot",
-      description: "This third-party-API-work-day-scheduler uses the moment.js library to work with datea and time, This app features dynamically updated HTML and CSS powered by JQuery. This app will organize your daily-work-life, simply add an event in the corresponding hour and click on the save.",
+      description: "This third-party-API-work-day-scheduler-solo-project uses the moment.js library to work with datea and time, This app features dynamically updated HTML and CSS powered by JQuery. This app will organize your daily-work-life, simply add an event in the corresponding hour and click on the save.",
       liveURL: "https://andres-abreu.github.io/WORK-DAY-SCHEDULER/",
       gitHub: "https://github.com/andres-abreu/WORK-DAY-SCHEDULER"
     },
@@ -65,7 +65,7 @@ function Portfolio() {
     {
       name: "Find It",
       id: "find-it-screen-shot",
-      description: "Find-it allows users to buy and sell products via the Find-it-market-place. Technologies in use are express-sessions, express-handle-bars, handle-bares, multer, mysqp2, sequelize and bycrypt for user security.",
+      description: "Find-it was a team-effort of 4, my initial duties or focus was with the database and minor front-end aesthetics using bootstrap and custom-CSS for the UI. This app allows users to buy and sell products via the Find-it-market-place. Technologies in use are express-sessions, express-handle-bars, handle-bares, multer, mysqp2, sequelize and bycrypt for user security.",
       liveURL: "https://limitless-beach-87360.herokuapp.com/",
       gitHub: "https://github.com/andres-abreu/FIND-IT"
     },
@@ -73,7 +73,7 @@ function Portfolio() {
     {
       name: "Race of the year",
       id: "race-of-the-year-photo",
-      description: "A REACT-based marathon-event-registration and support web-site using json-web-tokens for encryption, graphQL for fulfilling queries with existing data.",
+      description: "A REACT-based marathon-event-registration and support web-site built by a team of 4, my initial duties was with the data-base and minor front-end aesthetics using bootstrap and CSS for the UI-framework. This app is using json-web-tokens for encryption, graphQL for fulfilling queries with existing data.",
       liveURL: "https://https://ancient-sierra-07886.herokuapp.com/",
       gitHub: "https://github.com/andres-abreu/race-of-the-year"
     },
